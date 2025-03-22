1231821d5efca1e74512b6d07c87e0efabecb983
Matteo Zanella <matteo.zanella98@gmail.com>
2025-03-22 16:53:54 +0100

    feat: add \roll command for groups

--- bot/bot.py
+++ bot/bot.py
@@ -4,7 +4,7 @@ import os
 import traceback
 
 from telegram import Update
-from telegram.ext import Application, CommandHandler, MessageHandler, ContextTypes, filters
+from telegram.ext import Application, CommandHandler, MessageHandler, ContextTypes, filters, InlineQueryHandler
 from telegram.constants import ParseMode
 from constants import START_TEXT, INSTRUCTIONS_TEXT, RICKROLL_TEXT
 from dice_roller import roll_query
@@ -13,21 +13,32 @@ from dice_roller import roll_query
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(START_TEXT, parse_mode=ParseMode.MARKDOWN)
 
+
 async def instructions(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(INSTRUCTIONS_TEXT, parse_mode=ParseMode.MARKDOWN)
+
 
 async def rickroll(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(RICKROLL_TEXT, parse_mode=ParseMode.MARKDOWN)
 
+
 async def roll(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    message = update.message.text
-    await update.message.reply_text(roll_query(message), parse_mode=ParseMode.MARKDOWN_V2)
+    if context.args is None and update.message.text is not None:
+        dice_formula = update.message.text
+    else:
+        dice_formula = "".join(context.args)
+    await update.message.reply_text(
+        roll_query(dice_formula),
+        parse_mode=ParseMode.MARKDOWN_V2,
+    )
+
 
 async def app_process_update(app, event):
     async with app:
         data = json.loads(event["body"])
         update = Update.de_json(data=data, bot=app.bot)
         await app.process_update(update)
+
 
 def create_app():
     bot_token = os.getenv('BOT_TOKEN')
@@ -38,10 +49,14 @@ def create_app():
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("help", instructions))
     app.add_handler(CommandHandler("rickroll", rickroll))
+    app.add_handler(CommandHandler("roll", roll, has_args=True))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, roll))
+    app.add_handler(InlineQueryHandler(roll))
     return app
 
+
 APPLICATION = create_app()
+
 
 def lambda_handler(event, context):
     try:
